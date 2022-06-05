51005f9408d644c1095e77ea5c4f1532c085c83b
Abrazil <andreus.br@gmail.com>
2022-06-05 10:40:32 -0300

    Movendo as classes

--- src/main/java/com/BancoDigital/Main.java
+++ src/main/java/com/BancoDigital/Main.java
@@ -8,12 +8,15 @@ public class Main {
 
     public static void main(String[] args ){
 
-         Conta conta1= new ContaCorrente(500.00,1000.00);
-         Conta conta2= new ContaPoupanca(1000.00);
+//         Conta conta1= new ContaCorrente(500.00,1000.00);
+//         Conta conta2= new ContaPoupanca(1000.00);
+//
+//         conta1.sacar(200);
+//
+//         conta1.transferir(conta2,200.00);
 
-         conta1.sacar(200);
+        //System.out.printf("Teste");
 
-         conta1.transferir(conta2,200.00);
 
 
 
